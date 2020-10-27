0e84b2f1808ff011fd18df88243577fa3c5da744
takenoko <takenoko.tech@gmail.com>
2020-10-27 23:52:38 +0900

    add script.

---
+++ unity/Assets/UniLib/Scripts/Editor/ExportLib.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniLib.Scripts.Editor
+{
+    internal static class ExportLib
+    {
+        [MenuItem("UniLib/Export Android")]
+        public static void Build()
+        {
+            // Gather values from project
+            var scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+
+            // Define BuildPlayer Options
+            var buildOptions = new BuildPlayerOptions
+            {
+                scenes = scenes,
+                locationPathName = "Build",
+                target = BuildTarget.Android
+            };
+
+            // Perform build
+            var buildReport = BuildPipeline.BuildPlayer(buildOptions);
+
+            // Summary
+            var summary = buildReport.summary;
+            Debug.Log(summary);
+
+            // Result
+            var result = summary.result;
+            Debug.Log(result);
+        }
+    }
+}
