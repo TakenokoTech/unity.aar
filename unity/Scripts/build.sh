a4ee89cdd3eabb45001c4a2f0b121c4380a50fa7
takenoko <takenoko.tech@gmail.com>
2020-10-28 21:50:06 +0900

    add script.

---
+++ unity/Scripts/build.sh
@@ -0,0 +1,10 @@
+/Volumes/Transcend/Unity/2019.4.13f1/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-logfile - \
+-projectPath . \
+-quit \
+-buildTarget Android \
+-customBuildName Android \
+-customBuildTarget Android \
+-customBuildPath Build \
+-executeMethod UniLib.Scripts.Editor.ExportLib.Build
